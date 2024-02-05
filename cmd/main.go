bc887a0173277151388115c84d6676db51619295
Zhovtaniuk Maxym <zhovtyjshady@gmail.com>
2024-02-05 09:15:07 +0200

    test binance api

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"github.com/adshao/go-binance/v2"
+	"log"
+)
 
+func main() {
+	cl := binance.NewClient("", "")
+	do, err := cl.NewListPricesService().Do(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, sp := range do {
+		fmt.Println(sp.Symbol, sp.Price)
+	}
 }
